47fb3388a1d3bc599ff406f7e70b5f7cf620458d
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-06-15 14:59:38 +0200

    add beans env  and deploy script

--- bin/set_beas_env.sh
+++
@@ -1,5 +0,0 @@
-eb setenv POSTGRES_USERNAME=postgres
-eb setenv POSTGRES_PASSWORD=postgres
-eb setenv POSTGRES_DB=postgres
-eb setenv POSTGRES_HOST=database-1.cxx8mvvgngs2.us-east-1.rds.amazonaws.com
-eb setenv JWT_SECRET=very-secret
